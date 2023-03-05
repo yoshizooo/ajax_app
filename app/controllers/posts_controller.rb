569c6a15af1a4c8eb1706963e2b98ad5ce1edc7e
yoshizooo <y-i1996@yahoo.ne.jp>
2023-03-05 17:35:02 +0900

    JSでレスポンス処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,5 @@ class PostsController < ApplicationController
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
-    redirect_to action: :index
   end
 end
